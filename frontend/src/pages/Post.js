307a7e5c42d85e4457e672ea578b85e1ea7ad695
lam.chan_aexl <lamchan@agileexlab.net>
2022-05-01 16:50:04 +0800

    added comment right side

--- frontend/src/pages/Post.js
+++ frontend/src/pages/Post.js
@@ -2,15 +2,34 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from "axios";
 
+
 const Post = () => {
     let { id } = useParams();
+
     const [postObject, setPostObject] = useState({});
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState("");
 
     useEffect(() => {
         axios.get(`http://localhost:3001/posts/byId/${id}}`).then((respon) => {
-        setPostObject(respon.data);
+          setPostObject(respon.data);
         });
-    });
+
+        axios.get(`http://localhost:3001/comments/${id}}`).then((respon) => {
+          setComments(respon.data);
+          });
+    },[]);
+
+    const addComment = () =>{
+      axios.post("http://localhost:3001/comments", 
+      {commentBody: newComment, 
+        PostId: id})
+        .then((respon) => {
+          const commentToAdd = {commentBody: newComment};
+          setComments([...comments, commentToAdd]);
+          setNewComment("");
+      }) ;
+    }
 
   return (
     <div className='postPage'>
@@ -22,7 +41,18 @@ const Post = () => {
         </div>
       </div>
       <div className='rightSide'>
-        Comment Section
+        <div className='addCommentContainer'>
+          <input type="text" placeholder='Comments...' value={newComment} onChange={ (event) => setNewComment(event.target.value)}/>
+          <button onClick={ addComment }>Add Comment </button>
+        </div>
+        <div className='listOfComments'>
+          {comments.map((comments, key) => {
+            return (
+            <div className='comment'> 
+              { comments.commentBody }
+            </div>)
+          })}
+        </div>
       </div>
     </div>
   )
