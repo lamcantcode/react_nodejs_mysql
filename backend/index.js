75b9a57de317a2fb52cc9f0768f9bd82575d1822
lam.chan_aexl <lamchan@agileexlab.net>
2022-05-01 23:49:30 +0800

    version 1

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,9 @@ const postRouter = require('./routes/Posts');
 app.use("/posts", postRouter);
 const commentsRouter = require('./routes/Comments');
 app.use("/comments", commentsRouter);
+const usersRouter = require('./routes/Users');
+app.use("/auth", usersRouter);
+
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () => { // port 3001
